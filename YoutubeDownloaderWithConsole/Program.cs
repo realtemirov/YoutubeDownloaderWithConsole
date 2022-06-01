3506e8a82ab48587fcbb05028779dd0affffa36c
realtemirov <temirovjahongir2809@gmail.com>
2022-06-01 12:04:02 +0500

    Add project files.

---
+++ YoutubeDownloaderWithConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using VideoLibrary;
+
+namespace YoutubeDownloaderWithConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = Console.ReadLine();
+
+            var youtube = YouTube.Default;
+            var allVidoes = youtube.GetAllVideos(url);
+            var videos = allVidoes.FirstOrDefault(x => (x.Resolution == 720) && (x.Format == VideoFormat.Mp4));
+
+            byte[] bytres = videos.GetBytes();
+            File.WriteAllBytes("d:\\Jahongir\\" + videos.FullName + "-" + videos.Resolution + ".mp4", bytres);
+        }
+    }
+}
